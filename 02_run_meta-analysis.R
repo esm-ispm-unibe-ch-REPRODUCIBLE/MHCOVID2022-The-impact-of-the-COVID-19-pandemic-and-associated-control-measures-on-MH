e6314353d1a40655190617aeae4673c804d08c16
x4nd3r <anthraxander@gmail.com>
2022-07-20 12:14:11 +0200

    Add meta-analysis script, cleaned up to end of frequentist analysis

---
+++ 02_run_meta-analysis.R
@@ -0,0 +1,157 @@
+#-------------------------------------------------------------------------------
+# File  : 02_run_meta-analysis.R
+#
+# Takes clean, processed outcome data and performs meta-analysis. Creates funnel
+# and forest plots of the results, creating figures 2a and 2b from the paper.
+#-------------------------------------------------------------------------------
+# Perform frequentist pre-during analysis for all conditions
+data <- read.csv("data/dataset_alex.csv")
+data.prepost <- data %>%
+  group_by(record_id, condition) %>%
+  mutate(sum_prepandemic = sum(is_prepandemic)) %>%
+  filter(sum_prepandemic > 0) %>%
+  # TODO: calculate these in 01_process_data.R
+  mutate(se = sqrt(var)) %>%
+  mutate(log_mean_study_sample_size = log(mean_study_sample_size))
+
+# Subset the data by conditions, to create separate plots
+data.prepost.daa <- data.prepost %>%
+  filter(condition %in% c("Anxiety", "Depression"))
+data.prepost.psych <- data.prepost %>%
+  filter(condition == "Psychological distress")
+conditions <- c("Psychological distress", "Mental Wellbeing",
+                "Sleep disturbance", "Internet Gaming Disorder",
+                "Problematic smartphone-application use", "Life satisfaction",
+                "Problematic social media use", "Somatoform disorder")
+data.prepost.other <- data.prepost %>%
+  filter(condition %in% conditions)
+
+# Meta-analysis for Depression and Anxiety
+meta.prepost.daa <- metagen(TE = d,
+                            seTE = se,
+                            studlab = author_year,
+                            data = data.prepost.daa,
+                            subset = (is_prepandemic == 0),
+                            subgroup = condition,
+                            overall = TRUE,
+                            fixed = FALSE,
+                            prediction = TRUE,
+                            prediction.subgroup = TRUE)
+# Meta-analysis for other conditions
+meta.prepost.other <- metagen(TE = d,
+                              seTE = se,
+                              studlab = author_year,
+                              data = data.prepost.other,
+                              subset = (is_prepandemic == 0),
+                              subgroup = condition,
+                              overall = FALSE)
+# Meta-analysis for psychological distress
+meta.prepost.psych <- metagen(TE = d,
+                              seTE = se,
+                              studlab = author_year,
+                              data = data.prepost.psych,
+                              subset = (is_prepandemic == 0),
+                              subgroup = condition,
+                              overall = FALSE)
+
+# Draw funnel plot for anxiety and depression
+funnel(meta.prepost.daa, contour = c(0.9, 0.95, 0.99), xlab="SMD anxiety and depression")
+legend(-0.50, 0.02,
+       c("0.1 > p > 0.05", "0.05 > p > 0.01", "< 0.01"),
+       fill = c("darkgray", "gray", "lightgray"))
+# Draw funnel plot for psychological distress
+funnel(meta.prepost.psych,contour = c(0.9, 0.95, 0.99),xlab="SMD psychological distress")
+legend(-0.50, 0.02,
+       c("0.1 > p > 0.05", "0.05 > p > 0.01", "< 0.01"),
+       fill = c("darkgray", "gray", "lightgray"))
+
+# Create forest plot of pre-post pandemic meta-analysis in other conditions (Figure 2b)
+pdf(paste("Figure 2b Pre-post forest plot other conditions", ".pdf", sep=""), width = 11, height = 11)
+cols.forest <- c("studlab", "country", "population", "sample_size",
+                 "pop0_percent_female", "rob_info_bias", "rob_non_bias",
+                 "days_after_first")
+labs.forest <- c("Study", "Country", "Population", "Sample size ", "Fraction females ",
+                 "Information bias ", "Non-reponse bias ", "Days in pandemic")
+forest(meta.prepost.other,
+       test.overall = FALSE,
+       comb.fixed = FALSE,
+       print.byvar = TRUE,
+       overall  = FALSE,
+       weight.study = "same",
+       title = "Post-Pre SMD",
+       pooled.totals = FALSE,
+       xlab = paste("SMD"),
+       smlab = "SMD",
+       print.pval.Q = FALSE,
+       plotwidth = "6cm",
+       print.I2 = FALSE,
+       lwd = 1,
+       fontsize = 7,
+       fs.test.effect.subgroup = 0,
+       subgroup = FALSE,
+       fs.hetstat = 7,
+       just = "left",
+       addrow = FALSE,
+       leftcols = cols.forest,
+       leftlabs = labs.forest,
+       colgap = "0.01cm",
+       col.by = "black",
+       hetlab = "",
+       calcwidth.subgroup = FALSE,
+       overall.hetstat = FALSE,
+       hetstat = FALSE,
+       text.random.w = sapply(meta.prepost.other$k.w, paste, "observations"),
+       sort = days_after_first)
+dev.off()
+
+####### Perform BAYESIAN pre-during analysis for  Anxiety, Depression ------
+#source("Make pre-post Bayesian meta-analysis functions.R")
+#
+#prepostJAGSdata<-makeprepostJAGSdata(data=dataprepostB,studyid=record_id, y=d,var,covlabel="cov", corr=0.6)
+#prepostJAGSdata$Y1<-prepostJAGSdata$Y
+#prepostJAGSresults<-jags(data=prepostJAGSdata,inits=NULL,parameters.to.save = c("mu","muMH", "predSMD", "tau", "tauMH","predSMDMH"),model.file = prepostMHmodelRE,
+#                         n.chains=3,n.iter = 50000,n.burnin = 10000,DIC=F,n.thin = 3)
+#
+#mus=as.data.frame(t(dplyr::select(as.data.frame(t(prepostJAGSresults$BUGSoutput$summary)),starts_with("mu"))))
+#taus=as.data.frame(t(dplyr::select(as.data.frame(t(prepostJAGSresults$BUGSoutput$summary)),starts_with("tau"))))
+#predSMDs=as.data.frame(t(dplyr::select(as.data.frame(t(prepostJAGSresults$BUGSoutput$summary)),starts_with("predSMD"))))
+#prepostresults=rbind(mus,predSMDs,taus) %>% select(`50%`,`2.5%`,`97.5%`) %>% rename("Posterior Median"=`50%`, "lowCI"=`2.5%`,"highCI" =`97.5%` )
+#rm(taus,mus,predSMDs,d1,d2)
+#
+####Trick forest.meta to show the  the Bayesian results and create Figure 2a----
+#
+#meta.prepost.daaB<-meta.prepost.daa
+#
+#meta.prepost.daaB$TE.random <- prepostresults[3,1]
+#meta.prepost.daaB$lower.random <- prepostresults[3,2]
+#meta.prepost.daaB$upper.random <- prepostresults[3,3]
+#meta.prepost.daaB$lower.predict <- prepostresults[6,2]
+#meta.prepost.daaB$upper.predict <- prepostresults[6,3]
+#
+#meta.prepost.daaB$TE.random.w<- prepostresults[1:2,1]
+#meta.prepost.daaB$lower.random.w <- prepostresults[1:2,2]
+#meta.prepost.daaB$upper.random.w <- prepostresults[1:3,3]
+#meta.prepost.daaB$lower.predict.w <- prepostresults[4:5,2]
+#meta.prepost.daaB$upper.predict.w <- prepostresults[4:5,3]
+#
+#meta.prepost.daaB$tau.w<-prepostresults[7:8,1]
+#meta.prepost.daaB$tau<-prepostresults[9,1]
+#
+#meta.prepost.daaB$lower.tau.w<-prepostresults[7:8,2]
+#meta.prepost.daaB$upper.tau.w<-prepostresults[7:8,3]
+#meta.prepost.daaB$lower.tau<-prepostresults[9,2]
+#meta.prepost.daaB$upper.tau<-prepostresults[9,3]
+#
+#pdf(paste("Figure 2a Pre-post forest plot Anxiety and Depression",".pdf",sep=""), width = 11, height = 9)
+#forest(meta.prepost.daaB,test.overall=F, prediction=T, comb.common=F,print.byvar=T, overall =T,
+#       title="Post-Pre SMD",pooled.total=F, xlab=paste("SMD"),smlab="SMD",print.pval.Q=F,
+#       plotwidth="6cm",print.I2=F,lwd=1,fontsize=7,fs.test.effect.subgroup=0,
+#       fs.hetstat=7, just="left",addrow=F,
+#       leftcols = c("studlab","country","population","sample_size","pop0_percent_female", "rob_info_bias.x","rob_non_bias.x","days_after_first"),
+#       leftlabs=c("Study","Country", "Population","Sample size","Fraction females","Information bias","Non-reponse bias","Days in pandemic"),
+#       colgap="0.01cm",col.by="black",hetlab="",calcwidth.subgroup=T,print.tau=T,print.tau.ci=T,
+#       text.random.w=sapply(meta.prepost.daa$k.w, paste, "observations"), sort=days_after_first)
+#
+#dev.off()
+#
+#rm(d3,dataprepost, prepostJAGSdata,prepostJAGSresults, meta.prepost.daa,meta.prepost.daaB,meta.prepost.other,meta.prepost.psych)
